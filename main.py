9f73236d504ab300fcd9ca2049efd3ab2e980df2
unohayato <mamidori1p1p@gmail.com>
2022-09-21 16:46:34 +0900

    get json adress

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
 res = requests.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=1000001')
-print(res.status_code)
-print(res.text)
+res_json = res.json()
+results = res_json['results'][0]
+address = results['address1'] + results['address2'] + results['address3']
+print(address)
+
